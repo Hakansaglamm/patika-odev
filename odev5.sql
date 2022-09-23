6873225590605779c9d5f43ddfb6133e3ba126b8
Hakan Saglam <Hakan.Saglamm@outlook.com>
2022-09-23 12:21:48 +0300

    first commit

---
+++ odev5.sql
@@ -0,0 +1,18 @@
+1.
+SELECT title FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+LIMIT 5
+
+2.
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC 
+OFFSET 5
+LIMIT 5
+
+3.
+SELECT last_name,store_id FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4
